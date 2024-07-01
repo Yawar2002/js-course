efca659dd8af822cc2898b3d1d236e9ba24b802f
yawarali <yawaralibalti617@gmail.com>
2024-07-01 18:15:59 +0500

    first commit

---
+++ 01var.js
@@ -0,0 +1,31 @@
+// console.log("hello word");
+// let fullName ="hello"
+// console.log("fullName");
+// //fullName..full-name, full_name..FullName
+// function addThreeNumbers(num1, num2, num3) {
+//  return num1 + num2 + num3;
+// }
+// let c=addThreeNumbers(1, 2, 3)
+// console.log(c)
+// let d=12
+// console.log(d)
+// d = "hi"
+// console.log(d)
+// q=4
+// console.log(q)
+let fullName = "AL-Nafi";
+let email="Al.nafi@gmail.com";
+let password="123456";
+let confirmPassword="123456";
+let courseCount=0;
+let isLoggedInFormGoogle = false;
+console.log(`
+    username : ${fullName}
+    email:${email}
+    password:${password}
+    `);
+
+
+////////////////////////////////////////////////////////////////
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////////////////////////////////////////////
---
+++ 02optr.js
@@ -0,0 +1,12 @@
+// var num1 =5;
+// var num2 =6;
+// console.log(num1*num2);
+// var answer=num1>num2;
+// console.log(answer);
+
+var sellingPrice =120;
+var listingPrice =550;
+var discountPrice =((listingPrice-sellingPrice)/listingPrice)*100;
+console.log("Dis is",discountPrice);
+displaydisper=Math.round(discountPrice);
+console.log("Dis is",displaydisper + "% OFF");
