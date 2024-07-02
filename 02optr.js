7721f6b6ab1ab265a0c44c75d60d01d49c3274b7
yawarali <yawaralibalti617@gmail.com>
2024-07-02 13:27:00 +0500

    sec day

--- 02optr.js
+++ 02optr.js
@@ -10,3 +10,20 @@ var discountPrice =((listingPrice-sellingPrice)/listingPrice)*100;
 console.log("Dis is",discountPrice);
 displaydisper=Math.round(discountPrice);
 console.log("Dis is",displaydisper + "% OFF");
+
+var result = listingPrice>sellingPrice ;
+console.log(result);
+
+// if (0 === 1); { alert("hi") }
+
+// // equivalent to:
+
+// if (0 === 1) /*do nothing*/ ;
+// alert ("hi");
+// console.log("alert");
+////////////////////////////////////////////
+
+console.log(3.5+21);
+console.log(2024-1969);
+console.log(65/240);
+console.log(0.2708*100);
