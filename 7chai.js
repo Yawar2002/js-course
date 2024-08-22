bbbe95bf23321280958c1a6e7285307744e1a6bb
yawarali <yawaralibalti617@gmail.com>
2024-08-22 20:14:35 +0500

    ajka kaam

--- 7chai.js
+++ 7chai.js
@@ -1,5 +1,36 @@
 
-const accountId=345678;
-let email='alnafi.com';
-let city='Karachi';
-console.log(email,city,accountId);
+// const accountId=345678;
+// let email='alnafi.com';
+// let city='Karachi';
+// console.log(email,city,accountId);
+// const score = 100
+// const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
---
+++ string.js
@@ -0,0 +1,21 @@
+// let name="Yawar"
+// console.log(name)
+// const name1="Alnafi"
+// const repoCount = 50
+
+//console.log(name1 + repoCount + "value"); don't use this
+
+//console.log(`My name is ${name}, and count no is ${repoCount}`);
+
+const gameName = new String ('Yawaafi')
+// console.log(gameName[0]);
+// console.log(gameName.___proto___);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('f'));
+
+
+
+
+
